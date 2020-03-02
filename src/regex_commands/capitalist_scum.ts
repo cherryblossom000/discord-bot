559253e3b02b89a4d22730dead68a8ebe24ecac4
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-02 16:51:39 +1100

    improvement(capitalist scum): remove triggering on michael, jack, james etc

--- src/regex_commands/capitalist_scum.ts
+++ src/regex_commands/capitalist_scum.ts
@@ -2,7 +2,7 @@ import type PinguRegexCommand from '../types/PinguRegexCommand'
 
 const command: PinguRegexCommand = {
   regex:
-    /michael|michelle|sun|james|dong|ding-dong|ding\s*dong|jack|jcak|senior|capitalism|capitalist|scum|466937419233755146/i,
+    /capitalism|capitalist|scum|466937419233755146/i,
   regexMessage: 'Kill all the capitalist scum!'
 }
 
