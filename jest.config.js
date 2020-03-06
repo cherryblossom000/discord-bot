babd726e2d358b1cdc3264d640efb3f9a26bdf50
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-06 21:23:03 +1100

    test(jest config): add modulePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   globals: {'ts-jest': {packageJson: './package.json'}},
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.spec.ts']
