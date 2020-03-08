494d726e0c9bd4b15a196a6560c2ccf77db5c385
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-08 15:18:15 +1100

    build(push-docs.sh): fix commit message

--- scripts/push-docs.sh
+++ scripts/push-docs.sh
@@ -1,3 +1,3 @@
 set -e
-git commit -m "docs: update from github action $GITHUB_WORKFLOW #$GITHUB_RUN_NUMBER [skip ci]" assets/index.html README.md
+git commit -m "docs: update from travis build #$TRAVIS_BUILD_NUMBER [skip ci]" README.md
 git push "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu" HEAD:master
