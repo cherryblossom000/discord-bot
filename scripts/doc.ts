b3e4b3d8f510da2050aaa3f04cd0846c7058202f
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-08 16:19:59 +1100

    refactor(doc.ts): move html to template file

--- scripts/doc.ts
+++ scripts/doc.ts
@@ -1,45 +1,20 @@
-import fs, {readdirSync} from 'fs'
+import {promises, readdirSync} from 'fs'
 import {join} from 'path'
-import {promisify} from 'util'
 import MarkdownIt from 'markdown-it'
 import table from 'markdown-table'
+import upperFirst from 'lodash.upperfirst'
 import type {PinguCommand} from '../src/types'
 
+const {mkdir, readFile, writeFile} = promises
 const path = (p: string): string => join(__dirname, p)
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
-
-const template = (title: string, description: string, content: string): string => `<!DOCTYPE html>
-<html>
-  <head>
-    <title>${title}</title>
-    <meta name="description" content="${description}">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link id="favicon" rel="icon" href="/pingu.jpg" type="image/x-icon">
-    <style>body {padding: 1em}</style>
-    <link href="/github-markdown.css" rel="stylesheet">
-    <!-- Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137716576-3"></script>
-    <script>
-      window.dataLayer=window.dataLayer||[]
-      function gtag(){dataLayer.push(arguments)}
-      gtag('js',new Date)
-      gtag('config','UA-137716576-3')
-    </script>
-  </head>
-  <body class="markdown-body">${content}</body>
-</html>`
 
 const commandsPath = path('../dist/src/commands')
 const readmePath = path('../README.md')
 
-const writeHtml = async (p: string, title: string, description: string, md: string): Promise<void> =>
-  writeFile(path(`../dist/assets/html/${p}.html`), template(title, description, new MarkdownIt({html: true}).render(md)))
-
 ;(async (): Promise<void> => {
   const readme = readFile(readmePath)
+  const mkdirPromise = mkdir(path('../dist/assets/html'), {recursive: true})
+  const templatePromise = readFile(path('template.html'))
   const license = readFile(path('../LICENSE'))
   const changelog = readFile(path('../CHANGELOG.md'))
 
@@ -61,9 +36,23 @@ const writeHtml = async (p: string, title: string, description: string, md: stri
       ]
     ))
   const newReadme = (await readme).toString().replace(/(?<=## Documentation\n)[\s\S]+(?=\n\n## Links)/, table(docs))
-  const promises = [
-    writeFile(readmePath, newReadme),
+  const promises = [writeFile(readmePath, newReadme)]
 
+  await mkdirPromise
+  const template = (await templatePromise).toString()
+  const writeHtml = async (p: string, title: string, description: string, md: string): Promise<void> =>
+    writeFile(path(`../dist/assets/html/${p}.html`), template
+      .replace('[title]', title)
+      .replace('[description]', description)
+      .replace('[content]', new MarkdownIt({html: true}).render(md))
+    )
+  const writeOtherPage = async (p: string, md: Promise<Buffer>): Promise<void> => {
+    const P = upperFirst(p)
+    return writeHtml(p, `${P} - Comrade Pingu`, `${P} for Comrade Pingu`, `${(await md).toString()}
+#### [\u2190 back](/)`)
+  }
+
+  promises.push(...[
     // update index.html
     writeHtml('index', 'Comrade Pingu', 'Kill all the capitalist scum!', newReadme
       .replace('./assets/img', '')
@@ -71,12 +60,11 @@ const writeHtml = async (p: string, title: string, description: string, md: stri
       .replace('CHANGELOG.md', 'changelog')
     ),
 
-    // update license.html
-    writeHtml('license', 'License - Comrade Pingu', 'License for Comrade Pingu', (await license).toString()),
-
+    // update license.html and changelog.html
+    writeOtherPage('license', license),
     // update changelog.html
-    writeHtml('changelog', 'Changelog - Comrade Pingu', 'Changelog for Comrade Pingu', (await changelog).toString())
-  ]
+    writeOtherPage('changelog', changelog)
+  ])
 
   await Promise.all(promises)
 })()
