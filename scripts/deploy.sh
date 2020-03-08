734df86c080d9db039fad30d0934af1765b3f02c
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-08 14:32:25 +1100

    style(scripts): remove new lines [skip ci]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 set -e
-
 (
   cd dist
   # Copy files
--- scripts/push-docs.sh
+++ scripts/push-docs.sh
@@ -1,5 +1,3 @@
 set -e
-
-git add assets/index.html README.md
-git commit -m "docs: update from github action $GITHUB_WORKFLOW #$GITHUB_RUN_NUMBER [skip ci]"
+git commit -m "docs: update from github action $GITHUB_WORKFLOW #$GITHUB_RUN_NUMBER [skip ci]" assets/index.html README.md
 git push "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu" HEAD:master
