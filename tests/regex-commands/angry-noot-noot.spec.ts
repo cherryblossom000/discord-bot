ab67bf595f4f1b493a50f1f9fe38bfd2ff515a1d
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-21 17:53:44 +1100

    test(angry noot noot): add

---
+++ tests/regex-commands/angry-noot-noot.spec.ts
@@ -0,0 +1,22 @@
+import rewire from 'rewire'
+const regex = rewire('../../dist/src/regex-commands/angry-noot-noot').__get__<RegExp>('regex')
+
+describe('angry-noot-noot', () => {
+  describe('regex', () => {
+    [
+      'pingu is bad', 'pingu bad',
+      'pingu is very bad',
+      'pingu is not good',
+      'pingu is not very good',
+      'bad pingu',
+      'very bad pingu',
+      'not good pingu',
+      'pingu sucks'
+    ].forEach(string => it(string, () => {
+      expect(regex.test(string)).toBe(true)
+    }))
+    ;['good pingu', 'pingu', 'pingu is good', 'pingu is very good'].forEach(string => it(string, () => {
+      expect(regex.test(string)).toBe(false)
+    }))
+  })
+})
