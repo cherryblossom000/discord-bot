5391e3f164c36133c0b3a66191772256d9c54ad4
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-23 10:49:23 +1100

    fix(iwmelc): fix command not working

--- src/commands/iwmelc.ts
+++ src/commands/iwmelc.ts
@@ -8,6 +8,6 @@ export default {
   description: 'Gets the meme that shows that \u{201c}noot noot\u{201d} in Pingu means \u{201c}i will murder every last ' +
     'capitalist\u{201d} in English.',
   execute: async message => {
-    await message.channel.send(new MessageAttachment(join(__dirname, '../../assets/iwmelc.jpg')))
+    await message.channel.send(new MessageAttachment(join(__dirname, '../../assets/img/iwmelc.jpg')))
   }
 } as Command
