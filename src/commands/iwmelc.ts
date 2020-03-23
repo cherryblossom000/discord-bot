5c51fddae7401d06bc75c4f6068bccc68dbeb55d
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-24 01:29:43 +1100

    docs(iwmelc): make quotation marks single

--- src/commands/iwmelc.ts
+++ src/commands/iwmelc.ts
@@ -6,8 +6,8 @@ import type {Command, GuildMessage} from '../types'
 export default {
   name: 'iwmelc',
   aliases: ['iwillmurdereverylastcapitalist'],
-  description: 'Gets the meme that shows that \u201cnoot noot\u201d in Pingu means \u201ci will murder every last ' +
-    'capitalist\u201d in English.',
+  description: 'Gets the meme that shows that \u2018noot noot\u2019 in Pingu means \u2018i will murder every last ' +
+    'capitalist\u2019 in English.',
   execute: async message => {
     if (message.guild && !checkPermissions(message as GuildMessage, 'ATTACH_FILES')) return
     await message.channel.send(new MessageAttachment(join(__dirname, '../../assets/img/iwmelc.jpg')))
