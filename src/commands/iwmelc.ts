fa093c68cb8a5736ccf2aa43383796023776623a
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-24 01:19:44 +1100

    refactor(iwmelc): remove unnecessary braces around escapes

--- src/commands/iwmelc.ts
+++ src/commands/iwmelc.ts
@@ -6,8 +6,8 @@ import type {Command, GuildMessage} from '../types'
 export default {
   name: 'iwmelc',
   aliases: ['iwillmurdereverylastcapitalist'],
-  description: 'Gets the meme that shows that \u{201c}noot noot\u{201d} in Pingu means \u{201c}i will murder every last ' +
-    'capitalist\u{201d} in English.',
+  description: 'Gets the meme that shows that \u201cnoot noot\u201d in Pingu means \u201ci will murder every last ' +
+    'capitalist\u201d in English.',
   execute: async message => {
     if (message.guild && !checkPermissions(message as GuildMessage, 'ATTACH_FILES')) return
     await message.channel.send(new MessageAttachment(join(__dirname, '../../assets/img/iwmelc.jpg')))
