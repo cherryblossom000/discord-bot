e525cec1a199e8d99770ccd4c85ea7140c0b2c77
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-23 19:51:40 +1100

    build(push-docs.sh): update to push to correct branch

--- scripts/push-docs.sh
+++ scripts/push-docs.sh
@@ -1,3 +1,3 @@
 set -e
 git commit -m "docs: update from travis build #$TRAVIS_BUILD_NUMBER [skip ci]" README.md
-git push "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu" HEAD:master
+git push "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu" HEAD:$TRAVIS_BRANCH
