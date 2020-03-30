3a17f90c4d2caa538840f2d761ee9f29f61c29f5
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-31 01:33:15 +1100

    build: add remove-prod-check script

---
+++ scripts/remove-prod-check.ts
@@ -0,0 +1,103 @@
+import {join} from 'path'
+import {promises, statSync} from 'fs'
+import {ancestor} from 'acorn-walk'
+import {parse} from 'acorn'
+import type {
+  BinaryExpression, Expression, Identifier, IfStatement, Statement, VariableDeclarator, VariableDeclaration
+} from 'estree'
+
+const {readdir, readFile, writeFile} = promises
+
+const removeProdCheck = (source: string): string => {
+  const isNodeEnv = (node: Expression): boolean => node.type === 'MemberExpression' && !node.computed &&
+    node.object.type === 'MemberExpression' && !node.object.computed &&
+    node.object.object.type === 'Identifier' && node.object.object.name === 'process' &&
+    (node.object.property as Identifier).name === 'env' &&
+    (node.property as Identifier).name === 'NODE_ENV'
+
+  const isProduction = (node: Expression): boolean => node.type === 'Literal' && node.value === 'production'
+
+  const isCheckingIfProd = (node: BinaryExpression): boolean => ['==', '!=', '===', '!=='].includes(node.operator) &&
+    (isNodeEnv(node.left) && isProduction(node.right) || isNodeEnv(node.right) && isProduction(node.left))
+
+  const isProd = (node: BinaryExpression): boolean => ['==', '==='].includes(node.operator)
+
+  const isProdVars: {
+    /** Whether the variable is `true` if it is production. */
+    [variable: string]: boolean
+  } = {}
+
+  type WithRange<T = {}> = T & {range: [number, number]}
+
+  let newSource = source
+  let offset = 0
+
+  const replaceText = (original: WithRange, replacement: string): void => {
+    newSource = newSource.slice(0, original.range[0] + offset) +
+      replacement +
+      newSource.slice(original.range[1] + offset)
+    offset += replacement.length - (original.range[1] - original.range[0])
+  }
+
+  const remove = (node: WithRange): void => replaceText(node, '')
+
+  const replace = (original: WithRange, replacement: WithRange): void =>
+    replaceText(original, newSource.slice(...replacement.range.map(p => p + offset)))
+
+  ancestor(parse(source, {ranges: true, locations: true}), {
+    IfStatement(n) {
+      const node = n as unknown as WithRange<IfStatement>
+      let isTestProd: boolean | undefined
+      if (node.test.type === 'BinaryExpression' && isCheckingIfProd(node.test)) isTestProd = isProd(node.test)
+      else if (node.test.type === 'Identifier') isTestProd = isProdVars[node.test.name]
+
+      if (typeof isTestProd !== 'undefined') {
+        const statement = (isTestProd ? node.consequent : node.alternate) as WithRange<Statement>
+        if (statement) replace(node, statement)
+        else remove(node)
+      }
+    },
+    VariableDeclarator(n, a) {
+      const node = n as unknown as WithRange<VariableDeclarator>,
+        ancestors = a as WithRange<acorn.Node>[],
+        declaration = ancestors[ancestors.length - 2] as unknown as WithRange<VariableDeclaration> & {
+          declarations: WithRange<VariableDeclarator>[]
+        },
+        {declarations} = declaration
+
+      if (node.init?.type === 'BinaryExpression' && isCheckingIfProd(node.init) && node.id.type === 'Identifier') {
+        isProdVars[node.id.name] = isProd(node.init)
+        if (declarations.length === 1) remove(declaration)
+        else {
+          const i = declarations.indexOf(node)
+          const next = declarations[i + 1]
+          remove({range: [
+            i === declarations.length - 1 ? declarations[i - 1].range[1] : node.range[0],
+            next ? next.range[0] : node.range[1]
+          ]})
+        }
+      }
+    }
+  })
+
+  return newSource
+}
+
+const walk = async (path: string): Promise<string[]> => {
+  const files = (await readdir(path)).map(file => join(path, file))
+  ;(await Promise.all(files
+    .filter(file => statSync(file).isDirectory())
+    .map(async file => walk(file)))
+  ).forEach(subFiles => files.push(...subFiles))
+  return files
+}
+
+walk(join(__dirname, '../dist/src')).then(async files => {
+  await Promise.all(files
+    .filter(file => file.endsWith('.js'))
+    .map(async file => {
+      const source = await readFile(file)
+      writeFile(file, removeProdCheck(source.toString()))
+    })
+  )
+})
