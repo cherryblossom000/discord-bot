0e8d428e0dda084de4982a2efbb368bd8258701c
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-03-31 01:33:41 +1100

    ci(dpeloy): do semantic-release before deploying to glitch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,17 @@
 set -e
+
+# semantic-release
+pnpx semantic-release
+
 (
   cd dist
   # Init repo
   git init
   git remote add origin "https://$GLITCH_TOKEN@api.glitch.com/git/comrade-pingu"
+  # Copy package.json because that may have changed due tom semantic-release
+  cp ../package.json package.json
   # Push to glitch
   git add .
   git commit -m "update from travis build #$TRAVIS_BUILD_NUMBER"
   git push -f origin master
 )
-# semantic-release
-pnpx semantic-release
