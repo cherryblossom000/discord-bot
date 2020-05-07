4eb62ee50b034f5a9b0ece7046136757702ecce0
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-05-07 11:41:58 +1000

    improvement(capitalist-scum): triggers on Elon Musk-related things

--- src/regex-commands/capitalist-scum.ts
+++ src/regex-commands/capitalist-scum.ts
@@ -2,7 +2,7 @@ import type {RegexCommand} from '../types'
 
 const _: RegexCommand = {
   regex:
-    /capitalism|capitalist|scum/ui,
+    /capitalism|capitalist|scum|elon|musk/ui,
   regexMessage: 'Kill all the capitalist scum!'
 }
 export default _
