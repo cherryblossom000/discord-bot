cd196bb4b0b4ff20a278c069a25bc0d1b084d48b
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-05-16 19:46:48 +1000

    improvement(ping): show websocket ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,9 +5,11 @@ const _: Command = {
   aliases: ['p'],
   description: 'Gets my current latency.',
   cooldown: 5,
-  async execute({channel, createdTimestamp}) {
-    await channel.send(`Noot noot!
-My current latency is ${Date.now() - createdTimestamp} ms.`)
+  async execute({channel, client, createdTimestamp}) {
+    const msg = await channel.send('Pinging\u2026')
+    await msg.edit(`Noot noot!
+Latency: ${msg.createdTimestamp - createdTimestamp} ms
+Websocket: ${client.ws.ping} ms`)
   }
 }
 export default _
