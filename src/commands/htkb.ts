8a64b954eee00827e88782738f7aee4caf242a96
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-07-03 09:53:11 +1000

    feat: add htkb command

---
+++ src/commands/htkb.ts
@@ -0,0 +1,15 @@
+import {join} from 'path'
+import {MessageAttachment} from 'discord.js'
+import {checkPermissions} from '../helpers'
+import type {Command} from '../types'
+
+const _: Command = {
+  name: 'htkb',
+  aliases: ['howtokissboy'],
+  description: 'Gets the image that shows how to kiss a boy.',
+  async execute(message) {
+    if (message.guild && !await checkPermissions(message, 'ATTACH_FILES')) return
+    await message.channel.send(new MessageAttachment(join(__dirname, '../../assets/img/htkb.png')))
+  }
+}
+export default _
