3b06f52c648d97ce41d7ad4cbe391d5d00d27261
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-07-08 10:56:53 +1000

    build: push to repl branch

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+set -e
+
+# semantic-release
+pnpx semantic-release
+
+(
+  cd dist
+
+  # Copy package.json because that could have changed due to semantic-release
+  cp ../package.json package.json
+  # Add .replit
+  echo "language = 'nodejs'" > .replit
+  echo "run = 'node src/server'" >> .replit
+
+  # Init repo
+  git init
+  git remote add origin "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu"
+  git checkout -b repl
+  # Push to repl branch
+  git add .
+  git commit -m "chore: update from travis build #$TRAVIS_BUILD_NUMBER"
+  git push -f origin repl
+)
