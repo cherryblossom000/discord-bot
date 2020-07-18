8b892d731826d2f9d40acfcd1d8b33173ed7f406
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-07-18 10:58:27 +1000

    refactor(capitalist-scum): use `capitalis(m|t)` instead of `capitalism|capitalist`

--- src/regex-commands/capitalist-scum.ts
+++ src/regex-commands/capitalist-scum.ts
@@ -2,7 +2,7 @@ import type {RegexCommand} from '../types'
 
 const command: RegexCommand = {
   regex:
-    /capitalism|capitalist|scum|elon|musk/ui,
+    /capitalis(m|t)|scum|elon|musk/ui,
   regexMessage: 'Kill all the capitalist scum!'
 }
 export default command
