e2914154ff44bbb1e1b66914fcb0c427bdf23002
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-10-24 09:05:37 +1100

    chore(commitlint): use @comrade-pingu/commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,11 @@
 
 /** @type {import('@commitlint/types').UserConfig} */
 const config = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['@comrade-pingu'],
+  rules: {
+    'breaking-change-exclamation-mark': [2, 'always'],
+    'breaking-change-footer': [2, 'always']
+  }
 }
 module.exports = config
