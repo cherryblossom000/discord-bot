4bdb077a65c68e96c555f6c43b4ad0c7d1b5aa50
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-10-24 02:12:05 +1100

    chore(bot/purgecss): add types for purgecss config

--- packages/bot/purgecss.config.js
+++ packages/bot/purgecss.config.js
@@ -1,6 +1,8 @@
 'use strict'
 
-module.exports = {
+/** @type {Exclude<Parameters<import('purgecss').PurgeCSS['purge']>[0], string | undefined>} */
+const config = {
   content: ['dist/assets/html/*.html'],
   css: ['assets/css/*.css']
 }
+module.exports = config
