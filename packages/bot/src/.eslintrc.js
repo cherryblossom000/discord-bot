9c4511c81034d1af637bc692b8f42f49021f95fa
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-01-12 17:36:58 +1100

    chore(eslint): use proper type-checking for `bot/src/.eslintrc.js`

--- packages/bot/src/.eslintrc.js
+++ packages/bot/src/.eslintrc.js
@@ -1,8 +1,10 @@
 'use strict'
 
-module.exports = {
+/** @type {import('eslint').Linter.Config & {parserOptions?: import('@typescript-eslint/parser').ParserOptions}} */
+const config = {
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname
   }
 }
+module.exports = config
