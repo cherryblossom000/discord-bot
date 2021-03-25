e23e0853e87c5400c9b5cb8f7f77407dcbed7a8e
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-03-25 14:50:51 +1100

    feat(bot/emoji): add support for emojis outside of the server

--- packages/bot/src/commands/emoji.ts
+++ packages/bot/src/commands/emoji.ts
@@ -9,23 +9,21 @@ const command: GuildOnlyCommand = {
   usage: `\`emoji\`
 The emoji to get the image of.`,
   async execute(message) {
-    // TODO: check for permissons
-    const {channel, content, guild} = message
-    const id = /<(?::a)?:[\w]+:(\d+)>/u.exec(content)?.[1]
-    if (id === undefined) {
+    const {channel, content} = message
+    const match = (/<(a?):[\w]+:(\d+)>/u as {
+      exec(string: string): (RegExpExecArray & [string, string, string]) | null
+    }).exec(content)
+    if (!match) {
       await message.reply('please provide an emoji!')
       return
     }
+    const [, animated, id] = match
 
-    // Maybe the bot went offline or something when an emoji was created
-    const emoji =
-      guild.emojis.cache.get(id) ?? (await guild.fetch()).emojis.cache.get(id)
-    if (!emoji) {
-      await message.reply(`${id} is not a valid emoji id!`)
-      return
-    }
-
-    await channel.send({files: [emoji.url]})
+    await channel.send({
+      files: [
+        `https://cdn.discordapp.com/emojis/${id}.${animated ? 'gif' : 'png'}`
+      ]
+    })
   }
 }
 export default command
