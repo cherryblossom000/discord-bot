f145266f66a15116492d491ad9b2427414b5d5d0
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-08-30 22:42:46 +1000

    feat(bot/capitalist-scum): trigger on `bourgeois`

--- packages/bot/src/regex-commands/capitalist-scum.ts
+++ packages/bot/src/regex-commands/capitalist-scum.ts
@@ -1,7 +1,7 @@
 import type {RegexCommand} from '../types'
 
 const command: RegexCommand = {
-  regex: /capitalis(?:m|t)|scum|\belons*\b|melon|musk/iu,
+  regex: /capitalis(?:m|t)|scum|\belons*\b|melon|musk|bourgeois/iu,
   regexMessage: 'Kill all the capitalist scum!'
 }
 export default command
