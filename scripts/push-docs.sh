44ecc298a93a6fea2decbbd21f1f0898aeb19fae
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-09-14 22:16:52 +1000

    ci(push-docs): exit successfully if no update to docs

--- scripts/push-docs.sh
+++ scripts/push-docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-git commit -m "docs: update from workflow run #$GITHUB_RUN_NUMBER [skip ci]" packages/bot/README.md
+git commit -m "docs: update from workflow run #$GITHUB_RUN_NUMBER [skip ci]" packages/bot/README.md || exit 0
 git push "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY"
