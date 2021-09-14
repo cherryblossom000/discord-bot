c0ed2a44c0f4d11df58307db158523cfec6b1d70
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-09-14 22:11:35 +1000

    ci: fix docs script

--- scripts/push-docs.sh
+++ scripts/push-docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-git commit -m "docs: update from travis build #$TRAVIS_BUILD_NUMBER [skip ci]" packages/bot/README.md
-git push "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu" HEAD:$TRAVIS_BRANCH
+git commit -m "docs: update from workflow run #$GITHUB_RUN_NUMBER [skip ci]" packages/bot/README.md
+git push "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY"
