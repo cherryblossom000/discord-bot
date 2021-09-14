b528755e1972d0f42c2002ad2c36ba736e105c6a
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-09-14 22:04:18 +1000

    ci: use GitHub Actions
    Move from Travis CI to GitHub Actions.
    
    Fixes #3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
 
-# semantic-release
-pnpx multi-semantic-release
-
 # Update package.json (could have changed due to semantic-release) and remove
 # devDependencies so they aren't installed on Repl.it
 node scripts/dist/update-package
@@ -20,10 +17,10 @@ run = 'node src/server'" > .replit
 
   # Init repo
   git init
-  git remote add origin "https://$GH_TOKEN@github.com/cherryblossom000/comrade-pingu"
+  git remote add origin "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY"
   git checkout -b repl
   # Push to repl branch
-  git add *.js .replit package.json **/*.js assets ':!**/*.DS_Store'
-  git commit -m "chore: update from travis build #$TRAVIS_BUILD_NUMBER"
+  git add *.js .replit package.json **/*.js assets
+  git commit -m "chore: update from workflow run #$GITHUB_RUN_NUMBER"
   git push -f origin repl
 )
