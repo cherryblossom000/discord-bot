299200a881fe660db9a1fb55d0e93dd526f7e5fa
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-09-20 11:53:09 +0000

    m

--- src/commands/slash/meme.js
+++ src/commands/slash/meme.js
@@ -3,8 +3,8 @@ import { checkPermissions } from '../../utils.js';
 const IWMELC = 'I will murder every last capitalist';
 const HTKB = 'how to kiss boy';
 const imagesFolder = new URL('../../../assets/img/', import.meta.url);
-const htkb = new URL('htkb.jpg', imagesFolder).pathname;
 const iwmelc = new URL('iwmelc.jpg', imagesFolder).pathname;
+const htkb = new URL('htkb.jpg', imagesFolder).pathname;
 const MEME = 'meme';
 const command = {
     data: new SlashCommandBuilder()
@@ -15,15 +15,19 @@ const command = {
         .setDescription('The meme to get.')
         .setRequired(true)
         .addChoices([
-        [IWMELC, iwmelc],
-        [HTKB, htkb]
+        [IWMELC, "iwmelc"],
+        [HTKB, "htkb"]
     ])),
     usage: `!${hyperlink(IWMELC, './assets/img/iwmelc.jpg')}<br><img src="./assets/img/htkb.jpg" alt="${HTKB}" width="320">`,
     async execute(interaction) {
         if (!(await checkPermissions(interaction, 'ATTACH_FILES')))
             return;
         await interaction.reply({
-            files: [interaction.options.getString(MEME, true)]
+            files: [
+                interaction.options.getString(MEME, true) === "iwmelc"
+                    ? iwmelc
+                    : htkb
+            ]
         });
     }
 };
