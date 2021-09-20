b36170d0b82ed4c4069be57f1769ada328ffe9a9
replit thing <replit@email.com>
2021-09-20 11:29:23 +0000

    fix pnpm finally; remove old files

--- src/regex-commands/capitalist-scum.js
+++
@@ -1,6 +0,0 @@
-const command = {
-    regex: /capitalis(?:m|t)|scum|\belons*\b|melon|musk|bourgeois/iu,
-    regexMessage: 'Kill all the capitalist scum!'
-};
-export default command;
-//# sourceMappingURL=capitalist-scum.js.map
--- src/regex-commands/noot-noot.js
+++
@@ -1,6 +0,0 @@
-const command = {
-    regex: /no{2,}t\s*no{2,}t/iu,
-    regexMessage: message => `Noot noot, comrade ${message.guild ? message.member.displayName : message.author.username}.`
-};
-export default command;
-//# sourceMappingURL=noot-noot.js.map
