f53283bea05c2a42a4a0c17a8c247e8c404c820b
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-12-27 22:06:16 +1100

    refactor: simplify mathjax typings

--- packages/bot/src/mathjs.d.ts
+++ packages/bot/src/mathjs.d.ts
@@ -2,12 +2,13 @@ declare module 'mathjs' {
   export const evaluate: math.MathJsStatic['evaluate']
   export const simplify: math.MathJsStatic['simplify']
   export class ResultSet<T> {
+    // TODO: fix this bug
     // eslint-disable-next-line @typescript-eslint/no-shadow -- not shadowing
     static fromJSON: <T>(json: {entries?: readonly T[]}) => ResultSet<T>
     entries: T[]
     type: 'ResultSet'
     isResultSet: true
-    constructor(entries?: readonly T[]): ResultSet
+    constructor(entries?: readonly T[])
     valueOf(): T[]
     toString(): string
     toJSON(): {mathjs: 'ResultSet'; entries: T[]}
