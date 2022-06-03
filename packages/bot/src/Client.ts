728bd10e95efe8a76b25ff4a17796c03781abc62
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2022-06-03 10:28:37 +1000

    style: fix lint errors

--- packages/bot/src/Client.ts
+++ packages/bot/src/Client.ts
@@ -12,7 +12,7 @@ import type {ReadonlyNonEmpty} from './utils'
 
 declare global {
   interface ArrayConstructor {
-    isArray(arg: unknown | readonly unknown[]): arg is readonly unknown[]
+    isArray(arg: readonly unknown[]): arg is readonly unknown[]
   }
 }
 
