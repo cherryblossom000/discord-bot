4aad06761421c6e2beeb872deacb6564a187dbb1
cherryblossom <cherryblossom0001234@gmail.com>
2022-08-25 22:12:20 +1000

    style: fix lint error

--- packages/bot/purgecss.config.cjs
+++ packages/bot/purgecss.config.cjs
@@ -1,6 +1,8 @@
 'use strict'
 
+/* eslint-disable jsdoc/valid-types -- valid */
 /** @type {Exclude<Parameters<import('purgecss').PurgeCSS['purge']>[0], string | undefined>} */
+/* eslint-enable jsdoc/valid-types */
 const config = {
 	content: ['dist/assets/html/*.html'],
 	css: ['assets/css/*.css']
