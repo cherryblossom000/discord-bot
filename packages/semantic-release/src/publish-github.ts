b74159aa466fa359c7161689d72c2582ac23c30b
cherryblossom <cherryblossom0001234@gmail.com>
2022-09-05 19:53:39 +1000

    chore: upgrade deps

--- packages/semantic-release/src/publish-github.ts
+++ packages/semantic-release/src/publish-github.ts
@@ -48,7 +48,6 @@ const publishGithub: PublishPlugin = async (pluginConfig, context) => {
 
 	debug('release object: %O', release)
 
-	// eslint-disable-next-line @typescript-eslint/prefer-optional-chain -- asserts assets isn't undefined
 	if (!assets || !assets.length) {
 		const {
 			data: {html_url: url, id: releaseId}
